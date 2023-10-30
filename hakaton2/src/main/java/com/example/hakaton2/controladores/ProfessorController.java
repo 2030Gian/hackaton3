87cad7829684502ba44dd31808230aa28401a9fd
Gian Marco Arteaga Álvarez <gian.arteaga@utec.edu.pe>
2023-10-30 08:04:11 -0500

    Controller

---
+++ hakaton2/src/main/java/com/example/hakaton2/controladores/ProfessorController.java
@@ -0,0 +1,34 @@
+package com.example.hakaton2.controladores;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.hakaton2.repositorios.ProfessorRepository;
+import com.example.hakaton2.entidades.Professor;
+
+import java.util.List;
+import java.util.Optional;
+@RestController("ProfessorController")
+@RequestMapping("/professor")
+public class ProfessorController {
+
+    @Autowired ProfessorRepository professorRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Professor>> professors(){
+        List<Professor> professors = professorRepository.findAll();
+        return new ResponseEntity<>(professors, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> professor(@RequestBody Professor professor){
+        professorRepository.save(professor);
+        return  ResponseEntity.status(201).body("Create professor");
+
+
+    }
+
+    
+}
---
+++ hakaton2/src/main/java/com/example/hakaton2/controladores/StudentController.java
@@ -0,0 +1,37 @@
+package com.example.hakaton2.controladores;
+
+
+import org.springframework.beans.factory.annotation.Autowired; // para inyectar el Jpa
+import org.springframework.http.HttpStatus;     // para el estado si es 200, 400, 500 etc
+import org.springframework.http.ResponseEntity; 
+import org.springframework.web.bind.annotation.*;
+
+import com.example.hakaton2.entidades.Student; 
+import com.example.hakaton2.repositorios.StudentRepository;
+
+import java.util.Optional;
+import java.util.List;
+
+@RestController("StudentController")
+@RequestMapping("/student")
+public class StudentController {
+
+    @Autowired StudentRepository studentRepository; // nos sirve para usar el Jpa para hacer consultas 
+    // a nuestra base de datos
+
+    @GetMapping
+    public ResponseEntity<List<Student>> students(){
+        List<Student> students = studentRepository.findAll();
+        return new ResponseEntity<>(students,HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> student(@RequestBody Student student){
+        studentRepository.save(student);
+        return ResponseEntity.status(201).body("create student");
+
+    }
+
+
+    
+}
