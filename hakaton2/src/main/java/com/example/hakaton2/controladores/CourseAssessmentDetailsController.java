dd2114b7a9cebb3c7dcd751e46d68ab72485fc8b
Gian Marco Arteaga Álvarez <gian.arteaga@utec.edu.pe>
2023-10-31 14:25:32 -0500

    PUT

--- hakaton2/src/main/java/com/example/hakaton2/controladores/CourseAssessmentDetailsController.java
+++ hakaton2/src/main/java/com/example/hakaton2/controladores/CourseAssessmentDetailsController.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import com.example.hakaton2.entidades.CourseAssessmentDetails;
 import com.example.hakaton2.repositorios.CourseAssessmentDetailsRepository;
+import com.example.hakaton2.entidades.Course;
 
-import com.example.hakaton2.entidades.CourseAssessment;
 import com.example.hakaton2.repositorios.CourseAssessmentRepository;
+import com.example.hakaton2.entidades.CourseAssessment;
 
 
 import com.example.hakaton2.repositorios.ProfessorRepository;
@@ -17,6 +19,16 @@ import com.example.hakaton2.entidades.Professor;
 
 import com.example.hakaton2.entidades.Student; 
 import com.example.hakaton2.repositorios.StudentRepository;
+
+import com.example.hakaton2.entidades.Periodo;
+import com.example.hakaton2.repositorios.PeriodoRepository;
+
+import com.example.hakaton2.entidades.Course;
+import com.example.hakaton2.repositorios.CourseRepository;
+
+import com.example.hakaton2.entidades.CourseType;
+import com.example.hakaton2.repositorios.CourseTypeRepository;
+
 
 import java.util.List; 
 import java.util.Optional;
@@ -33,6 +45,14 @@ public class CourseAssessmentDetailsController {
 
     @Autowired
     private ProfessorRepository professorRepository;
+
+    @Autowired CourseAssessmentRepository courseAssessmentRepository;
+
+    @Autowired PeriodoRepository periodoRepository;
+
+    @Autowired CourseRepository courseRepository;
+
+    @Autowired CourseTypeRepository courseTypeRepository;
 
     @GetMapping
     public ResponseEntity<List<CourseAssessmentDetails>> getCourseAssessmentDetails(){
@@ -100,8 +120,6 @@ public class CourseAssessmentDetailsController {
             existingCourseAssessmentDetails.setScore(courseAssessmentDetails.getScore());
             existingCourseAssessmentDetails.setSection(courseAssessmentDetails.getSection());
             existingCourseAssessmentDetails.setSectionGroup(courseAssessmentDetails.getSectionGroup());
-            existingCourseAssessmentDetails.setCourseAssessment(courseAssessmentDetails.getCourseAssessment());
-            existingCourseAssessmentDetails.setProfessor(courseAssessmentDetails.getProfessor());
 
             Optional<Student> optionalStudent = studentRepository.findById(courseAssessmentDetails.getStudent().getId());
             if (optionalStudent.isPresent()){
@@ -114,7 +132,51 @@ public class CourseAssessmentDetailsController {
             
             Optional<Professor> optionalProfessor = professorRepository.findById(courseAssessmentDetails.getProfessor().getId());
             if (optionalProfessor.isPresent()){
-                Professor exiProfessor = optionalProfessor.get();
+                Professor existingProfessor = optionalProfessor.get();
+                existingProfessor.setName(courseAssessmentDetails.getProfessor().getName());
+                existingProfessor.setLastName(courseAssessmentDetails.getProfessor().getLastName());
+                existingProfessor.setEmail(courseAssessmentDetails.getProfessor().getEmail());
+                existingProfessor.setFullName(courseAssessmentDetails.getProfessor().getFullName());
+                professorRepository.save(existingProfessor);
+            }
+
+            Optional<CourseAssessment> optionalCourseAssessment = courseAssessmentRepository.findById(courseAssessmentDetails.getCourseAssessment().getId());
+            if (optionalCourseAssessment.isPresent()){
+                CourseAssessment existingCourseAssessment = optionalCourseAssessment.get();
+                existingCourseAssessment.setTitle(courseAssessmentDetails.getCourseAssessment().getTitle());
+                existingCourseAssessment.setTipoNota(courseAssessmentDetails.getCourseAssessment().getTipoNota());
+                existingCourseAssessment.setNumNota(courseAssessmentDetails.getCourseAssessment().getNumNota());
+                existingCourseAssessment.setNomenclatura(courseAssessmentDetails.getCourseAssessment().getNomenclatura());
+
+                Optional<Periodo> optionalPeriodo = periodoRepository.findById(courseAssessmentDetails.getCourseAssessment().getPeriodo().getId());
+                if (optionalPeriodo.isPresent()){
+                    Periodo existingPeriodo = optionalPeriodo.get();
+                    existingPeriodo.setName(courseAssessmentDetails.getCourseAssessment().getPeriodo().getName());
+                    existingPeriodo.setCode(courseAssessmentDetails.getCourseAssessment().getPeriodo().getCode());
+                    periodoRepository.save(existingPeriodo);
+                }
+
+                Optional<Course> optionalCourse = courseRepository.findById(courseAssessmentDetails.getCourseAssessment().getCourse().getId());
+                if (optionalCourse.isPresent()){
+                    Course existingCourse = optionalCourse.get();
+                    existingCourse.setName(courseAssessmentDetails.getCourseAssessment().getCourse().getName());
+                    existingCourse.setCredits(courseAssessmentDetails.getCourseAssessment().getCourse().getCredits());
+                    existingCourse.setCode(courseAssessmentDetails.getCourseAssessment().getCourse().getCode());
+                    existingCourse.setHrGroup(courseAssessmentDetails.getCourseAssessment().getCourse().getHrGroup());
+                    existingCourse.setCycle(courseAssessmentDetails.getCourseAssessment().getCourse().getCycle());
+                    existingCourse.setVrGroup(courseAssessmentDetails.getCourseAssessment().getCourse().getVrGroup());
+
+                    Optional<CourseType> optionalCourseType = courseTypeRepository.findById(courseAssessmentDetails.getCourseAssessment().getCourse().getCourseType().getId());
+                    if (optionalCourseType.isPresent()){
+                        CourseType existingCourseType = optionalCourseType.get();
+                        existingCourseType.setName(courseAssessmentDetails.getCourseAssessment().getCourse().getCourseType().getName());
+                        courseTypeRepository.save(existingCourseType);
+                    }
+                    courseRepository.save(existingCourse);
+                }
+
+                courseAssessmentRepository.save(existingCourseAssessment);
+                
             }
             courseAssessmentDetailsRepository.save(existingCourseAssessmentDetails);
 
@@ -122,8 +184,7 @@ public class CourseAssessmentDetailsController {
         } else {
             return ResponseEntity.status(404).body("Not Found");
         }
-    }
-    
+    }  
 }
 
 
